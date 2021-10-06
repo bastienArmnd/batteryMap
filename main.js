86978e263250dd3bb8ec26b5a423137dea08e283
bastienArmnd <bastien.armandpro@gmail.com>
2021-10-06 18:54:12 +0800

    move dot point logic inside unique on-load method

--- main.js
+++ main.js
@@ -125,109 +125,107 @@ map.on('load', () => {
     offMouseHoverCountry('nickel-producers')
     offMouseHoverCountry('cobalt-producers')
 
-const size = 150;
-// This implements `StyleImageInterface`
-// to draw a pulsing dot icon on the map.
-const pulsingDot = {
-    width: size,
-    height: size,
-    data: new Uint8Array(size * size * 4),
-         
-    // When the layer is added to the map,
-    // get the rendering context for the map canvas.
-    onAdd: function () {
-    const canvas = document.createElement('canvas');
-    canvas.width = this.width;
-    canvas.height = this.height;
-    this.context = canvas.getContext('2d');
-    },
-         
-    // Call once before every frame where the icon will be used.
-    render: function () {
-    const duration = 1000;
-    const t = (performance.now() % duration) / duration;
-         
-    const radius = (size / 2) * 0.3;
-    const outerRadius = (size / 2) * 0.7 * t + radius;
-    const context = this.context;
-         
-    // Draw the outer circle.
-    context.clearRect(0, 0, this.width, this.height);
-    context.beginPath();
-    context.arc(
-    this.width / 2,
-    this.height / 2,
-    outerRadius,
-    0,
-    Math.PI * 2
-    );
-    context.fillStyle = `rgba(255, 200, 200, ${1 - t})`;
-    context.fill();
-         
-    // Draw the inner circle.
-    context.beginPath();
-    context.arc(
-    this.width / 2,
-    this.height / 2,
-    radius,
-    0,
-    Math.PI * 2
-    );
-    context.fillStyle = 'rgba(255, 100, 100, 1)';
-    context.strokeStyle = 'white';
-    context.lineWidth = 2 + 4 * (1 - t);
-    context.fill();
-    context.stroke();
-         
-    // Update this image's data with data from the canvas.
-    this.data = context.getImageData(
-    0,
-    0,
-    this.width,
-    this.height
-    ).data;
-         
-    // Continuously repaint the map, resulting
-    // in the smooth animation of the dot.
-    map.triggerRepaint();
-         
-    // Return `true` to let the map know that the image was updated.
-    return true;
-    }
+    const size = 150;
+    // This implements `StyleImageInterface`
+    // to draw a pulsing dot icon on the map.
+    const pulsingDot = {
+        width: size,
+        height: size,
+        data: new Uint8Array(size * size * 4),
+
+        // When the layer is added to the map,
+        // get the rendering context for the map canvas.
+        onAdd: function () {
+            const canvas = document.createElement('canvas');
+            canvas.width = this.width;
+            canvas.height = this.height;
+            this.context = canvas.getContext('2d');
+        },
+
+        // Call once before every frame where the icon will be used.
+        render: function () {
+            const duration = 1000;
+            const t = (performance.now() % duration) / duration;
+
+            const radius = (size / 2) * 0.3;
+            const outerRadius = (size / 2) * 0.7 * t + radius;
+            const context = this.context;
+
+            // Draw the outer circle.
+            context.clearRect(0, 0, this.width, this.height);
+            context.beginPath();
+            context.arc(
+                this.width / 2,
+                this.height / 2,
+                outerRadius,
+                0,
+                Math.PI * 2
+            );
+            context.fillStyle = `rgba(255, 200, 200, ${1 - t})`;
+            context.fill();
+
+            // Draw the inner circle.
+            context.beginPath();
+            context.arc(
+                this.width / 2,
+                this.height / 2,
+                radius,
+                0,
+                Math.PI * 2
+            );
+            context.fillStyle = 'rgba(255, 100, 100, 1)';
+            context.strokeStyle = 'white';
+            context.lineWidth = 2 + 4 * (1 - t);
+            context.fill();
+            context.stroke();
+
+            // Update this image's data with data from the canvas.
+            this.data = context.getImageData(
+                0,
+                0,
+                this.width,
+                this.height
+            ).data;
+
+            // Continuously repaint the map, resulting
+            // in the smooth animation of the dot.
+            map.triggerRepaint();
+
+            // Return `true` to let the map know that the image was updated.
+            return true;
+        }
     };
-         
-    map.on('load', () => {
+
     map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
-         
+
     map.addSource('dot-point', {
-    'type': 'geojson',
-    'data': {
-    'type': 'FeatureCollection',
-    'features': [
-    {
-    'type': 'Feature',
-    'geometry': {
-    'type': 'Point',
-    'coordinates': [0, 25] // icon position [lng, lat]
-    }
-    }
-    ]
-    }
+        'type': 'geojson',
+        'data': {
+            'type': 'FeatureCollection',
+            'features': [
+                {
+                    'type': 'Feature',
+                    'geometry': {
+                        'type': 'Point',
+                        'coordinates': [0, 25] // icon position [lng, lat]
+                    }
+                }
+            ]
+        }
     });
     map.addLayer({
-    'id': 'layer-with-pulsing-dot',
-    'type': 'symbol',
-    'source': 'dot-point',
-    'layout': {
-    'icon-image': 'pulsing-dot'
-    }
+        'id': 'layer-with-pulsing-dot',
+        'type': 'symbol',
+        'source': 'dot-point',
+        'layout': {
+            'icon-image': 'pulsing-dot'
+        }
     });
     }
-    ,)}
 ,);
 
 // var bbox_nickel = turf.bbox(JSON.parse("./geojson/nickel-producers-countries.geojson").features[0].geometry.coordinates);
-var bbox_nickel = turf.extent(map.querySourceFeatures('nickel-producers'));
+// var bbox_nickel = turf.extent(map.querySourceFeatures('nickel-producers'));
 // var coordinates_nickel = bbox_nickel.features[0].geometry.coordinates;
 
 function fitNickel() {
