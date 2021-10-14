b0a70ba7cd2e7ab7b15aab6b9031a952fb120630
victo <victoriendelangue@gmail.com>
2021-10-14 09:23:59 +0200

    tried the toggle bar raw code from stackoverflow

--- main.js
+++ main.js
@@ -150,4 +150,14 @@ map.on('load', () => {
         .setPopup(popup) // sets a popup on this marker
         .addTo(map);
     }
+,
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+,   
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
 );
