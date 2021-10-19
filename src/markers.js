2832386d84fd2e4d237a5e9352eea4313bb604e5
bastienArmnd <bastien.armandpro@gmail.com>
2021-10-19 13:32:23 +0800

    fix bug when loading multiple layers

--- src/markers.js
+++ src/markers.js
@@ -1,36 +1,37 @@
 
 export function generateMarkers(map, openSidebar) {
 
-    function loadMineLayer(mineralName){
-        map.loadImage(
-            './img/'+ mineralName + '-icon.png',
-            (error, image) => {
-                if (error) throw error;
-    
-                // Add the image to the map style.
-                map.addImage(mineralName, image);
-    
-                // Add a data source containing one point feature.
-                map.addSource('point', {
-                    'type': 'geojson',
-                    'data': './geojson/markers/mines/'+ mineralName +'.geojson'
-                });
-    
-                // Add a layer to use the image to represent the data.
-                map.addLayer({
-                    'id': 'points',
-                    'type': 'symbol',
-                    'source': 'point', // reference the data source
-                    'minzoom': 0,
-                    'layout': {
-                        'icon-image': mineralName, // reference the image
-                        'icon-size': 0.1
-                    }
-                });
-            }
-        );
-    }
+    const layers = [
+        {geojson: './geojson/markers/gigafactories/gigafactory.geojson', url :  './img/gigafactory-icon.png', id: 'gigafactory'},
+        {geojson: './geojson/markers/mines/nickel.geojson', url: './img/nickel-icon.png', id: 'nickel'}
+    ]
 
-    // loadMineLayer('nickel');
-    loadMineLayer('gigafactory');
+    Promise.all(
+        layers.map(img => new Promise((resolve, reject) => {
+            map.loadImage(img.url, function (error, image) {
+                map.addImage(img.id, image)
+                resolve();
+            })
+        }))
+    )
+    .then(
+        layers.forEach(img => {
+        // Add a data source containing one point feature.
+        map.addSource(img.id, {
+            'type': 'geojson',
+            'data': img.geojson
+        }),
+
+        // Add a layer to use the image to represent the data.
+        map.addLayer({
+            'id': img.id,
+            'type': 'symbol',
+            'source': img.id, // reference the data source
+            'layout': {
+                'icon-image': img.id, // reference the image
+                'icon-size': 0.1,
+                'icon-allow-overlap': true
+            }
+        })
+    }));
 }
